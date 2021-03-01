b62546303646d7249cda568d03f25840d6f23e18
Filip-si <filip270697@gmail.com>
2021-03-01 15:19:10 +0100

    web app with login, register, sending email and add/display news/icon

--- src/main/java/com/infomax/web/converters/ImageConverter.java
+++
@@ -1,9 +0,0 @@
-package com.infomax.web.converters;
-
-import java.util.Base64;
-
-public class ImageConverter {
-    public String getImageData(byte[] byteData){
-        return Base64.getMimeEncoder().encodeToString(byteData);
-    }
-}
